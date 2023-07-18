1497bbe8425e54fb4296fd95a0f013406cc041f6
aamirphp <71968386+aamirphp@users.noreply.github.com>
2023-07-18 06:38:57 -0700

    Delete DesignWaves directory

--- DesignWaves/js/kHvfE1AHwUVc9pieEc2pm-_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Finspiration","\u002Fpublished\u002F[docId]","\u002Fpublished_mobile\u002F[docId]","\u002Ftemplates","\u002Ftemplates\u002F[id]","\u002Ftemplates\u002Fcategories\u002F[categorySlug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
